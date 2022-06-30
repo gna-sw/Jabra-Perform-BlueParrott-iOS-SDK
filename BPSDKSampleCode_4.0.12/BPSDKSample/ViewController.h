1109156252c47d406488d63efa2dd75d391694a9
alanjslattery <alan.slattery@gmail.com>
2022-06-30 10:13:42 +0100

    Adding iOS SDK 4.0.12

---
+++ BPSDKSampleCode_4.0.12/BPSDKSample/ViewController.h
@@ -0,0 +1,31 @@
+//
+//  ViewController.h
+//
+//  Copyright (c) 2019 Fenzar Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <BPHeadset.h>
+
+@interface ViewController : UIViewController <BPHeadsetListener>
+@property (weak, nonatomic) IBOutlet UIButton *connectButton;
+@property (weak, nonatomic) IBOutlet UIButton *sdkModeButton;
+@property (weak, nonatomic) IBOutlet UIButton *bondableButton;
+@property (weak, nonatomic) IBOutlet UITextView *logTextView;
+@property (weak, nonatomic) IBOutlet UIButton *clearButton;
+@property (weak, nonatomic) IBOutlet UILabel *logLabel;
+@property (weak, nonatomic) IBOutlet UITextField *enterpriseKeyGetField;
+@property (weak, nonatomic) IBOutlet UILabel *statusLabel;
+@property (weak, nonatomic) IBOutlet UIView *enterpriseView;
+@property (weak, nonatomic) IBOutlet UITextField *enterpriseValueField;
+@property (weak, nonatomic) IBOutlet UITextField *enterpriseKeyField;
+- (IBAction)entSetButtonTouched:(id)sender;
+- (IBAction)entGetButtonTouched:(id)sender;
+- (IBAction)entGetAllButtonTouched:(id)sender;
+
+- (IBAction)connectButtonTouched:(id)sender;
+- (IBAction)clearButtonTouched:(id)sender;
+- (IBAction)sdkModeButtonTouched:(id)sender;
+- (IBAction)bondableButtonTouched:(id)sender;
+@end
+
