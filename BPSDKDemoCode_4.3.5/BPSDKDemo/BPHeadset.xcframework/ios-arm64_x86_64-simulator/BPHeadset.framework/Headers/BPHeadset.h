a967d366f01e6713b29c3222dfe209e356e2ee6d
alanjslattery <alan.slattery@gmail.com>
2023-11-23 12:30:16 +0000

    BlueParrott iOS SDK v4.3.5
    Included in this release:
    - BP_iOS_SDK_4.3.5  - Folder containing the SDK Framework file and a docs folder with generated API documentation.
    - BPSDKSampleCode_4.3.5 - Folder containing an XCode project for the SDK Sample App
    - This release notes file
    
    New in 4.3.5
    - Added customerUUID setting to the SDK. This is a unique ID that is assigned to each account/customer and must be set during the development and release of your application.
    - Added support for remoteLogging to allow for improved support in troubleshooting while developing and after release.

---
+++ BPSDKDemoCode_4.3.5/BPSDKDemo/BPHeadset.xcframework/ios-arm64_x86_64-simulator/BPHeadset.framework/Headers/BPHeadset.h
@@ -0,0 +1,16 @@
+//
+//  BPHeadset.h
+//
+//  Copyright  [2017-2023] GN Audio A/S. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BPHeadset.
+FOUNDATION_EXPORT double BPHeadsetVersionNumber;
+
+//! Project version string for BPHeadset.
+FOUNDATION_EXPORT const unsigned char BPHeadsetVersionString[];
+
+#import <BPHeadset/BPHeadsetNative.h>
+
